8a5312fd8d22f8d3aeb1cfaffadd1157bfc22782
Alex Kyriazis <akyriaz@MacBook-Pro.local>
2017-12-13 19:06:41 -0800

    adding files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('a.page-scroll').click(function(){
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+    return false;
+});
